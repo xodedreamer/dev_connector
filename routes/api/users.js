6de4a31abd15a85364759eb3702afbb4c6eff6ca
Lindokuhle Khumbulani <Lindokuhle.Khumbulani@RTT.co.za>
2019-07-23 16:48:53 +0200

    added model, started user reg

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route       GET api/users
-// @desc        Test route
+const user = require('../../models/User');
+// @route       POST api/users
+// @desc        Register user
 // @access      Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include email').isEmail(),
+    check(
+      'password',
+      'Password must be at lease 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+    // Check if user exists
+
+    // Get users gravatar
+
+    //Encrypt password
+
+    //Return jsonwebtoken
+
+    res.send('User route');
+  }
+);
 
 module.exports = router;
