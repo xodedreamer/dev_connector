6a82d48fa7502098138bcdfecead7443c06193c8
Lindokuhle Khumbulani <Lindokuhle.Khumbulani@RTT.co.za>
2019-07-24 10:40:54 +0200

    inserted first user, hashed password, returned gravatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
-const user = require('../../models/User');
+const User = require('../../models/User');
 // @route       POST api/users
 // @desc        Register user
 // @access      Public
@@ -18,22 +20,44 @@ router.post(
       'Password must be at lease 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
     const { name, email, password } = req.body;
-    // Check if user exists
 
-    // Get users gravatar
+    try {
+      let user = await User.findOne({ email });
 
-    //Encrypt password
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // Get users gravatar
 
-    //Return jsonwebtoken
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'nm'
+      });
 
-    res.send('User route');
+      user = new User({ name, email, avatar, password });
+
+      //Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      //Return jsonwebtoken
+
+      res.send('User registered');
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
